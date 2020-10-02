04be2be2653b2806b60e5a4636cc27add33f87ee
Arturo B Martín <arturobm776@gmail.com>
2020-10-02 13:41:26 +0200

    Commit por si quiero seguir desarrollando en casa

---
+++ views/index.html.php
@@ -0,0 +1,50 @@
+<?php include 'skeleton/head.html.php'; ?>
+
+    <main id="main">
+      <article id="presentation_container">
+        <h1 id="presentation_text">CUIDA EL PLANETA</h1>
+        <a id="presentation_button" href="#">Leer m&aacute;s</a>
+      </article>
+
+      <article id="subtitle_container">
+        <h2 class="subtitle">OBJETIVOS</h2>
+      </article>
+      <article id="goals_container">
+        <section class="goal_view">
+          <a href="#">
+            <img class="image" src="/img/lynx_pardinus.jpg" alt="lynx_pardinus">
+          </a>
+          <h3 class="goal_title">Salvar al lince ib&eacute;rico</h3>
+          <div class="goal_text">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            <p class="text_wrapper"></p>
+            <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          </div>
+        </section>
+        <section class="goal_view">
+          <a href="#">
+            <img class="image" src="/img/plastic_in_the_ocean.jpg" alt="plastic_in_the_ocean">
+          </a>
+          <h3 class="goal_title">Limpiar los oc&eacute;anos</h3>
+          <div class="goal_text">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            <p class="text_wrapper"></p>
+            <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          </div>
+        </section>
+        <section class="goal_view">
+          <a href="#">
+            <img class="image" src="/img/reforestation.jpg" alt="reforestation">
+          </a>
+          <h3 class="goal_title">Reforestar los bosques</h3>
+          <div class="goal_text">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            <p class="text_wrapper"></p>
+            <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          </div>
+        </section>
+      </article>
+      <article class="article_wrapper"></article>
+    </main>
+
+<?php include 'skeleton/foot.html.php'; ?>
